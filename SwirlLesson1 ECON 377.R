6ff600e5b7496220232b07ff979da48775f35f7a
Jordi Domenech Castillo <jordidomenechcastillo@gmail.com>
2026-08-24 16:57:38 -0400

    Add files via upload

---
+++ SwirlLesson1 ECON 377.R
@@ -0,0 +1,36 @@
+# Swirl lesson 1
+install.packages('swirl')
+library(swirl) # Load swirl package
+install_course_github('isaaccloh','377_swirl')
+
+#start swirl
+swirl()
+
+#last name, first initial
+domenechj
+1
+1
+1
+
+5 + 7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z,555,z)
+z * 2 + 1000
+my_sqrt <- sqrt(z - 1)
+3
+print(my_sqrt)
+my_sqrt
+my_div <- z / my_sqrt
+1
+my_div
+recycling <- c(1, 2, 3, 4)
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
+2
