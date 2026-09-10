516e74145241bdc6b93784da284d46f65288c280
Jordi Domenech Castillo <jordidomenechcastillo@gmail.com>
2026-09-10 19:52:56 -0400

    Add files via upload

---
+++ Swirl lesson 4.R
@@ -0,0 +1,23 @@
+swirl()
+library(swirl)
+swirl()
+domenechj
+1
+4
+num_vect <- c(0.5,55,-10,6)
+tf <- num_vect < 1
+2
+tf
+num_vect >= 6
+1
+my_char <- c("My", "name", "is")
+print(my_char)
+my_char
+paste(my_char, collapse = " ")
+my_name <- c(my_char, "Jordi")
+my_name
+paste(my_name, collapse = " ")
+paste("Hello","world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+1
